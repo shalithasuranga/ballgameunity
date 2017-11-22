b62f70ec3bd2fc1b32c5de0b50a3a8cf58822743
Shalitha Suranga <shalithasuranga@gmail.com>
2017-11-22 07:43:36 +0530

    Adding files :rocket:

---
+++ Assets/BallController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class BallController : MonoBehaviour {
+
+    public Rigidbody ball;
+    private int score = 0;
+    private float timer = 60;
+    public Text scoreLabel;
+    public Text timeLabel;
+    public GameObject table;
+
+	// Use this for initialization
+	void Start () {
+	    
+    }
+
+    void updateUIScore() {
+        scoreLabel.text = "Score : " + score;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        timeLabel.text = "Time : " + (int)timer;
+        if (table.transform.position.y > ball.transform.position.y) {
+            Application.LoadLevel("Gameover");
+            
+        }
+        print(timer);
+        if (timer < 0) {
+            Application.LoadLevel("Gameover");
+        }
+	}
+
+    void FixedUpdate() {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 mov = new Vector3(h,0,v);
+        //print(h + " " + v);
+        ball.AddForce(mov);
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        if (other.tag.Equals("collect"))
+        {
+            
+            score++;
+            updateUIScore();
+        }
+        else if (other.tag.Equals("wrong")) {
+            score = (score - 1) < 0 ? 0 : (score - 1);
+            updateUIScore();
+            ball.AddForce(new Vector3(2,5,0) * 50);
+        }
+    }
+}
---
+++ Assets/CameraScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+    public GameObject ball;
+    public Vector3 diff;
+
+	// Use this for initialization
+	void Start () {
+        diff = this.transform.position - ball.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = ball.transform.position + diff;
+	}
+}
